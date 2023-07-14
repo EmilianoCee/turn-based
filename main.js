9eb951d98b01c52fedff38cc29c7db03bda0a350
Emiliano C <3cereceres@gmail.com>
2023-07-13 20:11:35 -0700

    file setup
    created each file

---
+++ main.js
@@ -0,0 +1,5 @@
+const canvas = document.querySelector(`canvas`);
+const ctx = canvas.getContext(`2d`);
+
+let width = canvas.width = window.innerWidth;
+let height = canvas.height = window.innerHeight;
