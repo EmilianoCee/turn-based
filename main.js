18ec14be821b780e686c9c26e315cf4295224234
Emiliano C <3cereceres@gmail.com>
2023-07-13 22:01:35 -0700

    began triangle
    points are being created in triangle formation, each formation is a quadrant of the board.

--- main.js
+++ main.js
@@ -1,49 +1,61 @@
 const canvas = document.querySelector(`canvas`);
 const ctx = canvas.getContext(`2d`);
 
-let width = canvas.width = window.innerWidth;
-let height = canvas.height = window.innerHeight;
+// set variables and canvas size as window height and width
+let canvasWidth = canvas.width = window.innerWidth;
+let canvasHeight = canvas.height = window.innerHeight;
 
-function Ball(x, y, color, size) {
+// defines Point object constructer 
+function Point(x, y, color, size) {
     this.x = x;
     this.y = y;
     this.color = color;
     this.size = size;
 };
 
-Ball.prototype.draw = function() {
+// 
+Point.prototype.draw = function() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
 };
 
-let balls = [];
+// empty array called points to store the multiple instances of point
+let points = [];
 
-for (let i = 0; i <= 2; i++) {
-    let ball = new Ball(
-        width/2,
-        height/2,
-        `rgb(0, 255, 0)`,
-        15*i
-    );
-    balls.push(ball);
+
+for (let k = 0; k <= 3; k++) {
+    let int = 1;
+    if(k%2) {
+        int *= -1;
+    }
+    for (let j = 0; j <= 2; j++) {
+        for (let i = 0; i <= j; i++) {
+            let point = new Point(
+                (canvasWidth/2) + (canvasWidth/20)*(i+1) * int,
+                (canvasHeight/2) + (canvasWidth/20)*(j+1) * int,
+                `rgb(${100*j}, 150, ${100*i})`,
+                15
+            );
+            points.push(point);
+        }
+    }
 }
-
-console.log(balls)
+console.log(points)
 
 function loop() {
     ctx.fillStyle = `rgba(0, 0, 0, 0.25)`;
-    ctx.fillRect(0, 0, width, height);
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
-    for (let i = 0; i < balls.length; i++) {
-        balls[i].draw();
+    for (let i = 0; i < points.length; i++) {
+        points[i].draw();
     }
     requestAnimationFrame(loop);
 }
 loop();
 
 window.addEventListener(`resize`, function() {
-    width = canvas.width = window.innerWidth;
-    height = canvas.height = window.innerHeight;
+    canvasWidth = canvas.width = window.innerWidth;
+    canvasHeight = canvas.height = window.innerHeight;
 })
